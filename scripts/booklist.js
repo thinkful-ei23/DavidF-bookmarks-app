db4af8bc4ab496676389a7a201f8e1e4755169c5
David Folks <aldwyn.wot@gmail.com>
2018-08-24 14:07:16 -0600

    updated styling

--- scripts/booklist.js
+++ scripts/booklist.js
@@ -1,8 +1,7 @@
 'use strict';
 /* global $, api, store */
 
-const bookList = (function(){
-  
+const bookList = (function() {
   function generateError(err) {
     let message = '';
     if (err.responseJSON && err.responseJSON.message) {
@@ -22,14 +21,18 @@ const bookList = (function(){
   function generateItemElement(item) {
     if (item.expanded) {
       return `
-      <li class="js-bookmark-item__expanded container" data-item-id="${item.id}">
+      <li class="js-bookmark-item__expanded container" data-item-id="${
+  item.id
+  }">
         <div class="name-rate">
           <span class="list-name col-6">${item.title}</span>
           <span class="list-rating rating col-6">${item.rating} Stars</span>
         </div>
         <div class="expandedlist">
-          <form id="js-edit-item-desc">
-            <textarea class="bookmark-desc" type="text" aria-label="Editable description" name="desc" cols="40" rows="3" required>${item.desc}</textarea>
+          <form id="js-edit-item-desc" class="desc-rate-update">
+            <textarea class="bookmark-desc" type="text" aria-label="Editable description" name="desc" cols="40" rows="3" required>${
+  item.desc
+  }</textarea>
             <select name="rating" aria-label="select to change rating">
               <option value="1">1 Star</option>
               <option value="2">2 Star</option>
@@ -39,28 +42,35 @@ const bookList = (function(){
             </select>
             <button type="submit" class="update-data">Update</button>
           </form>
-          <a href="${item.url}" class="button">Visit Page</a>
-          <button type="submit" class="list-delete">Remove</button>
-          <button class="list-collapse">Collapse</button>
+          <section class="visit-remove-collapse">
+            <a href="${item.url}" class="button">Visit Page</a>
+            <button type="submit" class="list-delete">Remove</button>
+            <button class="list-collapse">Collapse</button>
+          </section>
         </div>
       </li>`;
     } else {
       return `
     <li class="js-bookmark-item container" data-item-id="${item.id}">
       <div class="name-rate">
-        <span name="${item.title}" class="list-name">${item.title} (click to expand) </span>
-        <span name="${item.rating}" class="list-rating rating">${item.rating} Stars</span>
+        <span name="${item.title}" class="list-name">${
+    item.title
+  } (click to expand) </span>
+        <span name="${item.rating}" class="list-rating rating">${
+    item.rating
+  } Stars</span>
       </div>
     </li>
-    `;}
+    `;
+    }
   }
 
   function generateBookmarkItemsString(bookmarkList) {
-    const items = bookmarkList.map((item) => generateItemElement(item));
+    const items = bookmarkList.map(item => generateItemElement(item));
     return items.join('');
   }
 
-  function generateEditBox(){
+  function generateEditBox() {
     return `
       <form id="js-add-entry" class="container add-form" role="form">
         <input type="text" required aria-label="Enter Bookmark Name" name="title" class="name-entry" placeholder="Enter Bookmark Name Here">
@@ -72,16 +82,16 @@ const bookList = (function(){
           <option value="5">5 Star</option>
         </select>
         <input type="text" required aria-label="Enter URL" name="url" class="url-entry" placeholder="Enter URL Here">
-        <input type="text" required aria-label="Enter description" name="desc" class="desc-entry" placeholder="Enter description here">
+        <textarea class="desc-entry" type="text" aria-label="Enter description" name="desc" cols="40" rows="3" required placeholder="Enter Description"></textarea>
         <div class="error-container"></div>
         <button type="submit">Submit</button>
       </form>
     `;
   }
-      
+
   function render() {
     // Filter item list if store prop is true by item.checked === false
-    if (store.editMode){
+    if (store.editMode) {
       const html = generateEditBox();
       $('.placeholder').html(html);
       if (store.error) {
@@ -93,13 +103,13 @@ const bookList = (function(){
     } else {
       $('.placeholder').empty();
     }
-    let items = store.items;  
+    let items = store.items;
     items = store.items.filter(item => item.rating >= store.minVal);
     const bookListItemsString = generateBookmarkItemsString(items);
     $('.bookmark-list').html(bookListItemsString);
   }
-    
-  const handleEditMode = function(){
+
+  const handleEditMode = function() {
     $('.editMode').on('click', function(e) {
       e.preventDefault();
       store.editMode = !store.editMode;
@@ -107,7 +117,7 @@ const bookList = (function(){
     });
   };
 
-  const handleMinSelect = function () {
+  const handleMinSelect = function() {
     $('.min-set').on('change', event => {
       const newMin = parseInt($('.min-set').val());
       store.minVal = newMin;
@@ -115,22 +125,27 @@ const bookList = (function(){
     });
   };
 
-  const handleAddBookmark = function(){
-    $('.placeholder').on('submit','#js-add-entry', event => {
+  const handleAddBookmark = function() {
+    $('.placeholder').on('submit', '#js-add-entry', event => {
       event.preventDefault();
       const newItem = $(event.target).serializeJson();
-      {api.createItem(newItem, function(storeNew){
-        store.addItem(storeNew);
-        render();
-      }, (err) =>{
-        console.log(err);
-        store.setError(err);
-        render();
-      });
+      {
+        api.createItem(
+          newItem,
+          function(storeNew) {
+            store.addItem(storeNew);
+            render();
+          },
+          err => {
+            console.log(err);
+            store.setError(err);
+            render();
+          }
+        );
       }
-    });  
+    });
   };
-  
+
   function getItemIdFromElement(item) {
     return $(item)
       .closest('.js-bookmark-item')
@@ -143,17 +158,16 @@ const bookList = (function(){
       .data('item-id');
   }
 
-  const handleExpand = function(){
+  const handleExpand = function() {
     $('.bookmark-list').on('click', '.js-bookmark-item', event => {
       const id = getItemIdFromElement(event.currentTarget);
       const expandedObj = store.findById(id);
       expandedObj.expanded = true;
       render();
-    }
-    );
+    });
   };
 
-  const handleCollapse = function(){
+  const handleCollapse = function() {
     $('.bookmark-list').on('click', '.list-collapse', event => {
       const id = getItemIdFromExpandedElement(event.currentTarget);
       const collapsedObj = store.findById(id);
@@ -162,26 +176,26 @@ const bookList = (function(){
     });
   };
 
-  const handleDelete = function(){
+  const handleDelete = function() {
     $('.bookmark-list').on('click', '.list-delete', event => {
       const id = getItemIdFromExpandedElement(event.currentTarget);
-      api.deleteItem(id, function(){
+      api.deleteItem(id, function() {
         api.getitems(function(items) {
-          store.items=items;
+          store.items = items;
           render();
         });
       });
     });
   };
 
-  const handleEdit = function(){
+  const handleEdit = function() {
     $('.bookmark-list').on('submit', '#js-edit-item-desc', event => {
       event.preventDefault();
       const id = getItemIdFromExpandedElement(event.target);
       const newData = $(event.currentTarget).serializeJson();
       api.updateItem(id, newData, function() {
-        api.getitems(function(items){
-          store.items=items;
+        api.getitems(function(items) {
+          store.items = items;
           render();
         });
       });
